9bb23b0fff2ceae6207a556f59a36f19b04f52fa
aolusola <olusolaadeyemi8@gmail.com>
2026-05-14 16:32:45 +0100

    Add database config to my main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,35 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"github.com/emarkees/chi/internal/app"
-	"github.com/emarkees/chi/internal/routes"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/emarkees/chi/internal/app"
+	"github.com/emarkees/chi/internal/routes"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 
 	addr := flag.String("addr", ":8080", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://learn2earn:pass@localhost:5432/snippetbox", "Database connection string")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	/*
-		Initialize a new instance of our application struct, containing the
-		dependencies.
-	*/
+	// Connect to database (POOL)
+	dbpool, err := pgxpool.New(context.Background(), *dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer dbpool.Close()
 
+	// Test connection
+	err = dbpool.Ping(context.Background())
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// Application struct
 	app := &app.Application{
 		ErrorLog: errorLog,
 		InfoLog:  infoLog,
+		// DB: dbpool, // (recommended to add this)
 	}
-
-	// r := chi.NewRouter()
 
 	router := routes.SetUpRoutes(app)
 
@@ -40,6 +51,9 @@ func main() {
 	}
 
 	infoLog.Printf("Server is running on %s", *addr)
-	err := srv.ListenAndServe()
+
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
+
+func openDB()
