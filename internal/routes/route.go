1b856447f471caa89f2a3a48f1fe4cc8f69be55f
aolusola <olusolaadeyemi8@gmail.com>
2026-05-26 15:43:13 +0100

    Add middlewares, add secureHeader middlewares

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -2,13 +2,17 @@ package routes
 
 import (
 	"net/http"
-	"github.com/go-chi/chi/v5"
-	"github.com/emarkees/chi/internal/handlers"
+
 	"github.com/emarkees/chi/internal/app"
+	"github.com/emarkees/chi/internal/handlers"
+	"github.com/emarkees/chi/internal/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func SetUpRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.SecureHeaders)
 
 	// File is serve through the http.FileServer
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
